7ade058135d2a1334f28fa709f518cd6748b4d85
Sergio Paredes Nieves <sergio.paredes-beca@unir.net>
2026-01-21 09:25:21 +0100

    Migracion operativa y guardada

--- proyectoMigracion/obj/Debug/net8.0/proyectoMigracion.AssemblyInfo.cs
+++ proyectoMigracion/obj/Debug/net8.0/proyectoMigracion.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("proyectoMigracion")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+463905f57e3d178f6188eb4dc01524e41489b58b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+57735bdb90ba877a3082598036ef59891248db6f")]
 [assembly: System.Reflection.AssemblyProductAttribute("proyectoMigracion")]
 [assembly: System.Reflection.AssemblyTitleAttribute("proyectoMigracion")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
